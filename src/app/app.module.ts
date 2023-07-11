3be7019c0bd4e9f28aa8d222c99001f08e830dc1
IDGS-902-20001677 <20001677@alumnos.utleon.edu.mx>
2023-07-10 19:39:31 -0600

    CIVH 10-07-23 consumo servicios angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlumnosComponent } from './utl/alumnos/alumnos.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AgregarComponent } from './utl/agregar/agregar.component';
+import { EditarComponent } from './utl/editar/editar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    MenuComponent,
+    HomeComponent,
+    AgregarComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
